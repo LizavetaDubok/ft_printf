aae7b91f1c266444a63216a1b3e68f722706c3c7
Lizaveta <lizaveta@Lizavetas-MacBook-Air.local>
2026-08-13 14:14:19 +0200

    ...

--- ft_printf.h
+++ ft_printf.h
@@ -6,13 +6,12 @@
 /*   By: ldubok <ldubok@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 14:09:35 by ldubok            #+#    #+#             */
-/*   Updated: 2026/08/13 13:48:59 by ldubok           ###   ########.fr       */
+/*   Updated: 2026/08/13 14:14:05 by ldubok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <unistd.h>
-#include <limits.h>
 
 int	ft_printf(const char *args, ...);
 int	ft_parse_format(char format, va_list *args);
